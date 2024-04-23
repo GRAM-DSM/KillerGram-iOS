c1dc2d90e386dc7a1069916ecfd7b48919c9a319
HongSJae <gtw030488@gmail.com>
2024-04-23 19:26:56 +0900

    ✨ :: DI

--- Projects/Feature/RootFeature/Sources/RootComponent.swift
+++ Projects/Feature/RootFeature/Sources/RootComponent.swift
@@ -1,19 +1,16 @@
 import RootFeatureInterface
 import NeedleFoundation
 import SwiftUI
+import SplashFeatureInterface
 
 public protocol RootDependency: Dependency {
-//    var signinFactory: any SigninFactory { get }
-//    var splashFactory: any SplashFactory { get }
-//    var mainFactory: any MainFactory { get }
+    var splashFactory: any SplashFactory { get }
 }
 
 public final class RootComponent: Component<RootDependency>, RootFactory {
     public func makeView() -> some View {
         RootView(
-//            signinFactory: dependency.signinFactory,
-//            splashFactory: dependency.splashFactory,
-//            mainFactory: dependency.mainFactory
+            splashFactory: dependency.splashFactory
         )
     }
 }
