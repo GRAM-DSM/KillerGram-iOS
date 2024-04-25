f1b68c22bdec785efaad89f2f90c71f5c1ff8fc8
HongSJae <gtw030488@gmail.com>
2024-04-25 10:29:00 +0900

    ✨ :: Signup StudentID Publishing

--- Projects/Feature/SignupFeature/Sources/UserInfo/StudentID/SignupStudentIDComponent.swift
+++ Projects/Feature/SignupFeature/Sources/UserInfo/StudentID/SignupStudentIDComponent.swift
@@ -3,12 +3,14 @@ import NeedleFoundation
 import SignupFeatureInterface
 
 public protocol SignupStudentIDDependency: Dependency {
+    var signupGenderFactory: any SignupGenderFactory { get }
 }
 
 public final class SignupStudentIDComponent: Component<SignupStudentIDDependency>, SignupStudentIDFactory {
     public func makeView() -> some View {
         SignupStudentIDView(
-            viewModel: .init()
+            viewModel: .init(),
+            signupGenderFactory: dependency.signupGenderFactory
         )
     }
 }
--- Projects/Feature/SignupFeature/Sources/UserInfo/StudentID/SignupStudentIDViewModel.swift
+++ Projects/Feature/SignupFeature/Sources/UserInfo/StudentID/SignupStudentIDViewModel.swift
@@ -2,4 +2,11 @@ import BaseFeature
 import Combine
 
 final class SignupStudentIDViewModel: BaseViewModel {
+    @Published var studentID: String = ""
+
+    @Published var isNavigatedToGender: Bool = false
+
+    func nextButtonDidTap() {
+        self.isNavigatedToGender = true
+    }
 }
