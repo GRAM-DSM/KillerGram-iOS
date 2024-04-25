3cfc8c685fe182b83f64e1f88a84105e942a08da
HongSJae <gtw030488@gmail.com>
2024-04-25 10:28:20 +0900

    ✨ :: Email Auth Code Verify View Publishing

--- Projects/Feature/SignupFeature/Sources/EmailAuthCodeVerify/SignupEmailAuthCodeVerifyComponent.swift
+++ Projects/Feature/SignupFeature/Sources/EmailAuthCodeVerify/SignupEmailAuthCodeVerifyComponent.swift
@@ -3,13 +3,15 @@ import NeedleFoundation
 import SignupFeatureInterface
 
 public protocol SignupEmailAuthCodeVerifyDependency: Dependency {
+    var signupPasswordFactory: any SignupPasswordFactory { get }
 }
 
 public final class SignupEmailAuthCodeVerifyComponent: Component<SignupEmailAuthCodeVerifyDependency>,
                                                        SignupEmailAuthCodeVerifyFactory {
     public func makeView() -> some View {
         SignupEmailAuthCodeVerifyView(
-            viewModel: .init()
+            viewModel: .init(),
+            signupPasswordFactory: dependency.signupPasswordFactory
         )
     }
 }
--- Projects/Feature/SignupFeature/Sources/EmailAuthCodeVerify/SignupEmailAuthCodeVerifyView.swift
+++ Projects/Feature/SignupFeature/Sources/EmailAuthCodeVerify/SignupEmailAuthCodeVerifyView.swift
@@ -1,17 +1,68 @@
 import DesignSystem
 import SwiftUI
 import BaseFeature
+import ViewUtil
+import SignupFeatureInterface
 
 struct SignupEmailAuthCodeVerifyView: View {
+    private enum FocusField {
+        case authCode
+    }
+    @FocusState private var focusField: FocusField?
     @StateObject var viewModel: SignupEmailAuthCodeVerifyViewModel
 
+    private let signupPasswordFactory: any SignupPasswordFactory
+
     init(
-        viewModel: SignupEmailAuthCodeVerifyViewModel
+        viewModel: SignupEmailAuthCodeVerifyViewModel,
+        signupPasswordFactory: any SignupPasswordFactory
     ) {
         _viewModel = StateObject(wrappedValue: viewModel)
+        self.signupPasswordFactory = signupPasswordFactory
     }
 
     var body: some View {
-        Text("SignupEmailAuthCodeVerifyView")
+        VStack(spacing: 0) {
+            NavigationTitleView(
+                title: "인증번호를 입력해 주세요",
+                description: "입력해주신 이메일로 인증번호를 전송해 드렸어요"
+            )
+
+            VerifyCodeTextField(
+                $viewModel.authCode,
+                isError: viewModel.isErrorOccurred,
+                errorMessage: viewModel.errorMessage,
+                onCommit: viewModel.nextButtonDidTap
+            )
+            .focused($focusField, equals: .authCode)
+
+            HStack {
+                Text("5:00")
+                    .kgFont(.label, weight: .regular, color: .Greens.main)
+
+                Spacer()
+
+                Text("인증번호 재전송")
+                    .kgFont(.label, weight: .regular, color: .Grays.gray400)
+                    .underlineText(color: .Grays.gray400)
+            }
+            .padding(.vertical, 16)
+            .padding(.horizontal, 24)
+
+            Spacer()
+        }
+        .bottomButton(
+            text: "다음",
+            isEditing: focusField != nil,
+            isDisabled: viewModel.authCode.count < 4,
+            action: viewModel.nextButtonDidTap
+        )
+        .navigationBackButton()
+        .kgBackground()
+        .hideKeyboardWhenTap()
+        .navigate(
+            to: signupPasswordFactory.makeView().eraseToAnyView(),
+            when: $viewModel.isNavigatedToPassword
+        )
     }
 }
