c3fc06945efc7d79282c30c47051c81f21c5c15d
HongSJae <gtw030488@gmail.com>
2024-04-25 10:28:35 +0900

    ✨ :: Check Level Publishing

--- Projects/Feature/SignupFeature/Sources/UserInfo/CheckLevel/Components/LevelCheckCellView.swift
+++ Projects/Feature/SignupFeature/Sources/UserInfo/CheckLevel/Components/LevelCheckCellView.swift
@@ -1,19 +1,70 @@
-//
-//  LevelCheckCellView.swift
-//  SignupFeature
-//
-//  Created by 홍승재 on 4/25/24.
-//  Copyright © 2024 com.gram. All rights reserved.
-//
-
 import SwiftUI
+import DesignSystem
+
+enum PhysicalLevel {
+    case high
+    case middle
+    case low
+}
 
 struct LevelCheckCellView: View {
+    @Binding var selectedLevel: PhysicalLevel
+    let level: PhysicalLevel
+
+    init(_ level: PhysicalLevel, selectedLevel: Binding<PhysicalLevel>) {
+        _selectedLevel = selectedLevel
+        self.level = level
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            self.selectedLevel = level
+        } label: {
+            HStack(spacing: 8) {
+                Text(level.text)
+                    .kgFont(.m1, weight: .semiBold, color: .Grays.white)
+
+                Text(level.description)
+                    .kgFont(.m3, weight: .regular, color: .Grays.gray500)
+                    .lineLimit(1)
+
+                Spacer()
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 16)
+            .background(Color.Grays.gray1100)
+            .clipCornerRadius(8)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(
+                        self.selectedLevel == level ? Color.Greens.main : .clear,
+                        lineWidth: 1
+                    )
+            }
+        }
     }
 }
 
-#Preview {
-    LevelCheckCellView()
+extension PhysicalLevel {
+    var text: String {
+        switch self {
+        case .high:
+            "상"
+        case .middle:
+            "중"
+        case .low:
+            "하"
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .high:
+            "운동을 평소에 즐겨하며 잘 하는 편이에요"
+        case .middle:
+            "운동을 즐기진 않지만 못하지는 않아요"
+        case .low:
+            "운동을 별로 좋아하지 않아요"
+        }
+    }
 }
--- Projects/Feature/SignupFeature/Sources/UserInfo/CheckLevel/SignupCheckLevelViewModel.swift
+++ Projects/Feature/SignupFeature/Sources/UserInfo/CheckLevel/SignupCheckLevelViewModel.swift
@@ -2,4 +2,10 @@ import BaseFeature
 import Combine
 
 final class SignupCheckLevelViewModel: BaseViewModel {
+    @Published var selectedLevel: PhysicalLevel = .high
+    @Published var isSuccessToSignup: Bool = false
+
+    func nextButtonDidTap() {
+        self.isSuccessToSignup = true
+    }
 }
