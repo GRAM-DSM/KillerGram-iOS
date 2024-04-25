73aa598e9f4c091cbcc345cb7f557c34c2fb85fc
HongSJae <gtw030488@gmail.com>
2024-04-25 10:29:09 +0900

    ✨ :: Signup Gender Publishing

--- Projects/Feature/SignupFeature/Sources/UserInfo/Gender/SignupGenderComponent.swift
+++ Projects/Feature/SignupFeature/Sources/UserInfo/Gender/SignupGenderComponent.swift
@@ -3,12 +3,14 @@ import NeedleFoundation
 import SignupFeatureInterface
 
 public protocol SignupGenderDependency: Dependency {
+    var signupCheckLevelFactory: any SignupCheckLevelFactory { get }
 }
 
 public final class SignupGenderComponent: Component<SignupGenderDependency>, SignupGenderFactory {
     public func makeView() -> some View {
         SignupGenderView(
-            viewModel: .init()
+            viewModel: .init(),
+            signupCheckLevelFactory: dependency.signupCheckLevelFactory
         )
     }
 }
