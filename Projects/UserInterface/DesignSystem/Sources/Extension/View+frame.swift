f5190b9dbe5e2c41ae4df2252032dd7a5d77a3c8
HongSJae <gtw030488@gmail.com>
2024-05-12 21:57:30 +0900

    ✨ :: frame to size

--- Projects/UserInterface/DesignSystem/Sources/Extension/View+frame.swift
+++ Projects/UserInterface/DesignSystem/Sources/Extension/View+frame.swift
@@ -4,4 +4,8 @@ public extension View {
     func frame(_ size: CGFloat, alignment: Alignment = .center) -> some View {
         frame(width: size, height: size, alignment: alignment)
     }
+
+    func frame(size: CGSize, alignment: Alignment = .center) -> some View {
+        frame(width: size.width, height: size.height, alignment: alignment)
+    }
 }
